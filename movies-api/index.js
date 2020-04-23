c1106c6b7531e51137cda9c9567ec40abe94047e
luisma fernandez camacho <luismanuel.fernandez.camacho@gmail.com>
2020-04-23 09:54:25 +0200

    feat: add movies mock and get movies endpoint

--- movies-api/index.js
+++ movies-api/index.js
@@ -2,14 +2,9 @@ const express = require('express')
 const app = express()
 
 const { config } = require('./config')
+const moviesApi = require('./routes/movies')
 
-app.get('/', function (req, res) {
-  res.send('Hello world')
-})
-
-app.get('/json', function (req, res) {
-  res.json({ hello: 'Hello world' })
-})
+moviesApi(app)
 
 app.listen(config.port, function () {
   console.log(`Corriendo en el puerto ${config.port}`)
